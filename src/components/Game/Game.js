a7aeea092a954ddc279891d069a3325254db3b6e
Heymon <rcavalleiro@gmail.com>
2021-02-08 14:25:33 -0800

    adds basic lateral movement to player

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -11,6 +11,8 @@ class Game extends React.Component {
         playerTargetX: 0,
         // playerRotation: 0,
 
+        speed: 0
+
     }
 
     // gets the player position current position on screen
@@ -22,11 +24,88 @@ class Game extends React.Component {
     }
 
     componentDidMount () {
-        const playersize = this.getPlayerCurPos().width
+        const playerMeasurement = this.getPlayerCurPos();
         // sets player initial position
-        const initPosX = window.innerWidth/2 - playersize/2; //at the middle of the x AXIS
-        const initPosY = window.innerHeight - 18; //at the bottom of the screen// the subtration of 18 is to take in account the playr height
+        const initPosX = window.innerWidth/2 - playerMeasurement.width/2; //at the middle of the x AXIS
+        const initPosY = window.innerHeight - playerMeasurement.height; //at the bottom of the screen// the subtration of 18 is to take in account the playr height
         this.setState({playerTargetX: initPosX, playerTargetY: initPosY});
+        this.handleKeyboardMovement();
+
+    }
+
+    // they way movement works is by setting the target at either far right or far left on key down
+    //then on key up set the target to the players current position making them stop
+    lateralMove = (movementTarget) => {
+        this.setState({playerTargetX: movementTarget, speed: 1.5})
+    }
+
+    stopMovement = (pos, speed=1.5) => {
+        this.setState({playerTargetX: pos.x, speed});
+    }
+
+    handleKeyboardMovement = () => {
+
+        document.addEventListener('keydown', (e) => {
+            // console.log(e.code);
+            if(e.code === "ArrowRight") {
+                    //get the right border of the playable area in screen location
+                    const rightBorder = (window.innerWidth/2) + (320/2);// half of the screen plus halfof the playable area
+                    console.log(rightBorder)
+                    const curPos = this.getPlayerCurPos();
+                    // this is not working to well //couldnt figure out why; solution if they touch wall they die
+                    if( curPos.x + curPos.width >= rightBorder){//if the player reachs the border 
+                        console.log("should stop")
+                        console.log("touch wall die") //start some event here to say game over
+                        this.stopMovement({x: rightBorder - curPos.width, y: this.state.playerTargetY}, 0);//stop them there and raise the flag on the rightborder
+                        
+                        
+                    } else {
+                        this.lateralMove(window.innerWidth);// start to move right
+                    }
+            } else if(e.code === "ArrowLeft") {
+                    const leftBorder = (window.innerWidth/2) - (320/2);
+                    console.log(leftBorder);
+                    const curPos = this.getPlayerCurPos();
+                    if (curPos.x <= leftBorder) {
+                        console.log("should stop")
+                        console.log("touch wall die")// start some event here to say gameover
+                        this.stopMovement({x: leftBorder, y: this.state.playerTargetY}, 0);//stop them there and raise the flag on the leftborder
+                        
+                    } else {  
+                        this.lateralMove(0);// start to move left
+                    }
+
+            }
+        });
+
+        document.addEventListener('keyup', (e) => {
+            // console.log(e.code);
+            if(e.code === "ArrowRight") {
+                
+                const rightBorder = (window.innerWidth/2) + (320/2);
+                const curPos = this.getPlayerCurPos();
+                if( curPos.x + curPos.width >= rightBorder){
+                    console.log("game over")// game over here too just in case
+                    return this.stopMovement({x: rightBorder - curPos.width, y: this.state.playerTargetY}, 0);
+
+                } else {
+                    return this.stopMovement(this.getPlayerCurPos());
+                }
+               
+            } else if(e.code === "ArrowLeft") {
+                const leftBorder = (window.innerWidth/2) - (320/2);
+                const curPos = this.getPlayerCurPos();
+                if (curPos.x <= leftBorder) {
+                    console.log("game over") //game over here just in case
+                        return this.stopMovement({x: leftBorder, y: this.state.playerTargetY}, 0);
+                }else {
+                    this.stopMovement(this.getPlayerCurPos());
+                }
+                
+            }
+        });
+        
+        
 
     }
 
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,14 +1,14 @@
 import React from "react";
 
+import './Player.css';
+
 class Player extends React.Component{
     render() {
         return(
             <div id="player" style={{
-                position: "absolute",
-                width: "fit-content",
-                margin: 0,
                 // transform: `translateX(550px) translateY(840px)`,
                 transform: `translateY(${this.props.playerInfo.playerTargetY}px) translateX(${this.props.playerInfo.playerTargetX}px)` ,
+                transition: `${this.props.playerInfo.speed}s linear`
                 }}>
                     Player
                 </div>
