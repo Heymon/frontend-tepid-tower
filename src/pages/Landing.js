5ca92261f658b864301e700bbe9d3e6ad6c002a0
Heymon <rcavalleiro@gmail.com>
2021-02-08 12:04:47 -0800

    starts landing page with initial formatting

---
+++ src/pages/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Landing.css'
+
+
+function Landing() {
+    return (
+        <div className="landing">
+            <section>
+                <h1>Title Rght</h1>
+            </section>
+            <section>
+                <h1>Game Middle</h1>
+            </section>
+            <section>
+                <h1>Options Left</h1>
+            </section>
+        </div>
+    )
+    
+}
+
+export default Landing;
