838d383d80b8d56cda8934be0f7da98e1c68e1cd
Heymon <rcavalleiro@gmail.com>
2021-02-08 16:24:31 -0800

    adds jumping and landing mechanics to player

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -12,17 +12,19 @@ class Game extends React.Component {
         playerTargetX: 0,
         // playerRotation: 0,
 
-        speed: 0
+        playerLanded: true,
+        playerJumping: false,
+        playerFalling: false,
+
+        speed:0,
+        jumpGauge: 5,
+        gauge: null,
+
+        curPlatform: null,
 
     }
 
-    // gets the player position current position on screen
-    getPlayerCurPos = () => {
-        const playerEl = document.getElementById('player');
-        const playerCurPos = playerEl.getBoundingClientRect();
-        // console.log(playerCurPos);
-        return playerCurPos;
-    }
+    /* LIFECYCLE COMPONENT METHODS */
 
     componentDidMount () {
         const playerMeasurement = this.getPlayerCurPos();
@@ -32,6 +34,43 @@ class Game extends React.Component {
         this.setState({playerTargetX: initPosX, playerTargetY: initPosY});
         this.handleKeyboardMovement();
 
+    }
+
+    componentDidUpdate () {
+
+        console.log("update");
+
+        if (this.state.curPlatform !== null) {//if the player is on top of a platform
+            const playerCurPos = this.getPlayerCurPos();
+            const platformPos = this.state.curPlatform.getBoundingClientRect()
+            // after a movement check to see if he is still on top  of said platform
+            if ((
+                playerCurPos.x > platformPos.left &&     
+                playerCurPos.x < platformPos.right
+                ) || (
+                playerCurPos.x + 30 > platformPos.left && 
+                playerCurPos.x + 30 < platformPos.right
+                )) {
+                // console.log("should not fall");
+            } else {//if they are not on top make them fall and check possible landings 
+                // console.log("should fall");
+                if(!this.state.playerFalling && !this.state.playerJumping) {
+                    this.setState({playerFalling: true, playerLanded: false})
+                    this.checkAllPlatforms();
+                    this.land()
+                }
+            }
+        }
+    }
+
+    /* PLAYER MOVEMENT FUCNTIONS */
+
+    // gets the player position current position on screen
+    getPlayerCurPos = () => {
+        const playerEl = document.getElementById('player');
+        const playerCurPos = playerEl.getBoundingClientRect();
+        // console.log(playerCurPos);
+        return playerCurPos;
     }
 
     // they way movement works is by setting the target at either far right or far left on key down
@@ -44,14 +83,129 @@ class Game extends React.Component {
         this.setState({playerTargetX: pos.x, speed});
     }
 
+    // on key down the jump gauge starts at 5 and goes up to 10 depending when release//key up
+    // then similar to lateral movement the jump sets the target to a distance equivalent to the top of the screen
+    //(in this case 840px so if the player is at a platform the jump will be set at -840 from that position) and
+    // starts a timeout with the duration being multiplied by the gauge set on keydown so
+    // that instead of reaching the top of the screen when the timeout runs the procese of falling starts
+    jump = (amount) => {
+        // if not jumping or in the ground
+        if (this.state.playerJumping === false && this.state.playerLanded === true) {
+            
+            // then similar to lateral movement the jump sets the target to a distance equivalent to the top of the screen
+            const playerTarget = this.state.playerTargetY + amount;
+            this.setState({playerTargetY: playerTarget, speed: 1.5, playerJumping: true, playerLanded: false});
+
+            // starts a timeout with the duration being multiplied by the gauge set on keydown
+            setTimeout(() => {
+                this.checkAllPlatforms()//check all platform to define where player should land including the bottom
+                
+                this.land();//sets event Listener to confirm the landing
+            }, 48 * this.state.jumpGauge)
+
+        }
+    }
+
+    //sets event Listener to confirm the landing
+    land = () => {
+        const playerEl = document.getElementById('player');
+                playerEl.addEventListener('transitionend', () => {
+                    // console.log("touchedfloorDown");
+                    //when it reach the ground turns playerJumping to false
+                    
+                    this.setState({playerJumping: false, playerFalling: false, playerLanded: true, speed: 1.5});
+                    
+                }, {once: true});
+    }
+
+    checkLanding = (platformEl) => {
+
+        const playerCurPos = this.getPlayerCurPos();
+        const platformPos = platformEl.getBoundingClientRect();
+        
+        // console.log((playerCurPos.y + playerCurPos.height) <= platformPos.y);
+        // console.log(playerCurPos.x > platformPos.left); 
+        // console.log(playerCurPos.x < platformPos.right);
+        // console.log(playerCurPos.x + playerCurPos.width > platformPos.left );
+        // console.log(playerCurPos.x + playerCurPos.width < platformPos.right);
+        if ((playerCurPos.y + playerCurPos.height) <= platformPos.y) {//if the player is above the platform
+            //and the player is within the platform space
+            if ((
+                playerCurPos.x > platformPos.left &&     
+                playerCurPos.x < platformPos.right
+                ) || (
+                playerCurPos.x + playerCurPos.width > platformPos.left && 
+                playerCurPos.x + playerCurPos.width < platformPos.right
+                )) {
+                console.log("should land")
+                const landPos = (platformPos.top - (playerCurPos.height));//set the landing at the platform taking + the player height//read body
+                
+                const distance = landPos - playerCurPos.y;
+                if (distance < 100) {// if the distance between the player and where he is landing is small increase the veloctiy of landing
+                    console.log("faster" + distance)
+                    // this.setState({playerTargetY: landPos, speed: (distance < 30 ? 0.5 : 0.75 ), curPlatform: platformEl, playerFalling: true, playerJumping: false});
+                    this.setState({playerTargetY: landPos, speed: (Math.floor(distance/100)+0.1), curPlatform: platformEl, playerFalling: true, playerJumping: false});
+                    return true
+                }//if not just land normally
+                this.setState({playerTargetY: landPos, curPlatform: platformEl, playerFalling: true, playerJumping: false})
+                return true
+            }
+            return false //if the player is not landing on platform return false
+        }
+        return false //if the player is not landing on platform return false
+    }
+
+    checkAllPlatforms = () => {
+        const platformEls = document.querySelectorAll('.platform');
+        let isLandingOnPlatform = false;
+        for (let i = 0; i < platformEls.length; i++) {//for all the platforms
+            if (this.checkLanding(platformEls[i])) {//check if the player should land on them and if so make it the target
+                // console.log("chekc platforms");
+                    isLandingOnPlatform = true;
+                    break
+                }
+            }
+            if (!isLandingOnPlatform) {//if it didnt found a platform set landing to ground
+                const playerCurPos = this.getPlayerCurPos();
+                const towerBottom = window.innerHeight - playerCurPos.width;
+                const distance = towerBottom - playerCurPos.y;
+                if (distance < 100) {// if the distance between the player and where he is landing is small increase the veloctiy of landing
+                    console.log("faster" + distance)
+                    this.setState({playerTargetY: towerBottom, speed: (Math.floor(distance/100)+0.1), curPlatform: null, playerFalling: true, playerJumping: false});
+                    // this.setState({playerTargetY: towerBottom, speed: (distance < 30 ? 0.5 : 0.75 ), curPlatform: null, playerFalling: true, playerJumping: false});
+                    // console.log(this.state); 
+                    return true
+                } else {//if not just land normally
+                    console.log("slower" + distance)
+                    this.setState({playerTargetY: towerBottom, speed: 1.5, curPlatform: null, playerFalling: true, playerJumping: false});
+                } 
+            }
+
+    }
+
+    /* EVENT LISTENERS */
+
     handleKeyboardMovement = () => {
 
         document.addEventListener('keydown', (e) => {
             // console.log(e.code);
-            if(e.code === "ArrowRight") {
+            if(e.code === "Space") {
+                e.preventDefault();//so that page doesnt scroll down
+                if (this.state.gauge === null) {//if gauge havent already started aka if the player isnt already holding the key down
+                    this.setState({gauge: setInterval(() => {// set the interval to increase the gauge every 250ms
+                        if (this.state.jumpGauge < 10) {
+                            this.setState(prevState => {
+                                return {
+                                    jumpGauge: prevState.jumpGauge + 1
+                                }
+                            })
+                        }
+                    }, 250)})
+                }
+            } else if(e.code === "ArrowRight") {
                     //get the right border of the playable area in screen location
                     const rightBorder = (window.innerWidth/2) + (320/2);// half of the screen plus halfof the playable area
-                    console.log(rightBorder)
+                    // console.log(rightBorder);
                     const curPos = this.getPlayerCurPos();
                     // this is not working to well //couldnt figure out why; solution if they touch wall they die
                     if( curPos.x + curPos.width >= rightBorder){//if the player reachs the border 
@@ -65,7 +219,7 @@ class Game extends React.Component {
                     }
             } else if(e.code === "ArrowLeft") {
                     const leftBorder = (window.innerWidth/2) - (320/2);
-                    console.log(leftBorder);
+                    // console.log(leftBorder);
                     const curPos = this.getPlayerCurPos();
                     if (curPos.x <= leftBorder) {
                         console.log("should stop")
@@ -81,16 +235,24 @@ class Game extends React.Component {
 
         document.addEventListener('keyup', (e) => {
             // console.log(e.code);
-            if(e.code === "ArrowRight") {
+            if(e.code === "Space") {//when they let go of space 
+                clearInterval(this.state.gauge);//stop the intervals and the gauge
+                this.jump(-840);//do the jump
+                this.setState({gauge: null, jumpGauge: 5})//set the gauge to initial value
+            } else if(e.code === "ArrowRight") {
                 
                 const rightBorder = (window.innerWidth/2) + (320/2);
                 const curPos = this.getPlayerCurPos();
                 if( curPos.x + curPos.width >= rightBorder){
                     console.log("game over")// game over here too just in case
-                    return this.stopMovement({x: rightBorder - curPos.width, y: this.state.playerTargetY}, 0);
+                    this.stopMovement({x: rightBorder - curPos.width, y: this.state.playerTargetY}, 0);
 
                 } else {
-                    return this.stopMovement(this.getPlayerCurPos());
+                    this.stopMovement(this.getPlayerCurPos());
+                }
+                if(this.state.playerFalling === true ){
+                    this.checkAllPlatforms();
+
                 }
                
             } else if(e.code === "ArrowLeft") {
@@ -98,11 +260,14 @@ class Game extends React.Component {
                 const curPos = this.getPlayerCurPos();
                 if (curPos.x <= leftBorder) {
                     console.log("game over") //game over here just in case
-                        return this.stopMovement({x: leftBorder, y: this.state.playerTargetY}, 0);
+                    return this.stopMovement({x: leftBorder, y: this.state.playerTargetY}, 0);
                 }else {
                     this.stopMovement(this.getPlayerCurPos());
                 }
-                
+                if(this.state.playerFalling === true ){
+                    this.checkAllPlatforms();
+
+                }
             }
         });
         
