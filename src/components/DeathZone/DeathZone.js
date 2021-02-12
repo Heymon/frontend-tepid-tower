e65a1aa0a1e56d3b354019f330831a11ecab8287
Heymon <rcavalleiro@gmail.com>
2021-02-12 00:51:12 -0800

    stylizes deathzone

--- src/components/DeathZone/DeathZone.js
+++ src/components/DeathZone/DeathZone.js
@@ -1,12 +1,17 @@
 import React from "react"
-
+import fogo from '../../assets/Fogo-Animado-png.gif';
 import "./DeathZone.css"
 
 function DeathZone(props) {
 
     return (
-        <div className="deathzone" style ={(props.display ? {display: "block"}: {display: "none"})}>
-            death
+        <div className="deathzone" style ={(props.display ? {display: "flex"}: {display: "none"})}>
+            <div><img src={fogo}/></div>
+            <div><img src={fogo}/></div>
+            <div><img src={fogo}/></div>
+            <div><img src={fogo}/></div>
+            <div><img src={fogo}/></div>
+            <div><img src={fogo}/></div>
         </div>
     )
     
